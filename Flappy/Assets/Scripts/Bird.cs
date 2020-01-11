280408b570dd8eb76c2dce1fdbbf88a91c68baf1
MartinezRamirezMariaFernanda <fernanda.1d.jb@gmail.com>
2020-01-10 22:57:31 -0600

    Flappy

---
+++ Flappy/Assets/Scripts/Bird.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    private bool isDead;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+    public float upForce = 200f;
+
+    private RotateBird rotateBird;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rotateBird = GetComponent<RotateBird>();
+    }
+
+    // Use this for initialization
+    private void Start () {
+		
+	}
+
+    // Update is called once per frame
+    private void Update () {
+        if (isDead) return;
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(Vector2.up * upForce);
+            anim.SetTrigger("Flap");
+            SoundSystem.instance.PlayFlap();
+        }        
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
+        anim.SetTrigger("Die");
+        rotateBird.enabled = false;
+        GameController.instance.BirdDie();
+        rb2d.velocity = Vector2.zero;
+        SoundSystem.instance.PlayHit();
+    }
+}
